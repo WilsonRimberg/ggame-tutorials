45bf9658cbcfdbf90c3b7e10bd0b5d4252329c95
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-18 13:57:03 -0400

    Updated from Brython Server: 10/18/2016 1:57:03 PM

--- anotherone.py
+++ anotherone.py
@@ -1,1 +1,14 @@
-
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, CircleAsset
+PrettyColors={'Green':'0x00ff00', 'Red':'0xff000', 'Blue':'0x0000ff'}
+class Circle(App):
+    def __init__(self, radius):
+         super().__init__(radius)
+    radius=int(input("Radius?"))
+    black=Color(0,1)
+    red=Color(0xA62A2A)
+    ogline=Linestyle(1, black)
+    Circle_Asset=CircleAsset(radius, ogline, red)
+    Circle=Sprite(Circle_Asset, (100,100))
+        
+myapp = App(Circle)
+myapp.run()
